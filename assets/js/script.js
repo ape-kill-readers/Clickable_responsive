b9c6b60e9f6f9c19d185098de9c47cbf3a4c848a
Chuseshi-rookie <yu.nakamura.gamegod@gmail.com>
2024-01-26 11:00:22 +0900

    コードレビューからのアドバイスにて非同期処理からasync/awaitを使い同期処理に変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,30 @@ script.src =
 document.head.appendChild(script);
 
 // ライブラリの読み込みが完了したら、imageMapResize 関数を実行
-script.onload = function () {
+script.onload = async function () {
+  try {
+    // JSON ファイルを読み込み、座標データを設定
+    const response = await fetch("./data/coordinates.json");
+    const data = await response.json();
+
+    data.areas.forEach((area) => {
+      const areaElement = document.createElement("area");
+      areaElement.setAttribute("shape", area.shape);
+      areaElement.setAttribute("coords", area.coords.join(","));
+      areaElement.setAttribute("href", area.href);
+      areaElement.setAttribute("alt", area.alt);
+      // 他の属性も必要に応じて設定
+      document.querySelector("map").appendChild(areaElement);
+    });
+
+    // imageMapResize を実行
+    imageMapResize();
+  } catch (error) {
+    console.error("Error loading coordinates.json:", error);
+  }
+};
+
+/*script.onload = function () {
   // JSON ファイルを読み込み、座標データを設定
   fetch("./data/coordinates.json")
     .then((response) => response.json())
@@ -26,4 +49,4 @@ script.onload = function () {
       imageMapResize();
     })
     .catch((error) => console.error("Error loading coordinates.json:", error));
-};
+};*/
